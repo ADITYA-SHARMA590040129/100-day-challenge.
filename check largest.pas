1c2e2d77fc70cfd78d82928fc2fb62263e4ed4c7
ADITYA SHARMA (590040129) <satviksashwat321@gmail.com>
2026-09-20 20:32:06 +0530

    Add files via upload

---
+++ check largest.pas
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+int A,B,C;
+printf("enter three no");
+scanf("%d %d %d,&A &B &C");
+if(A>B&&A>C)
+printf("no 1 is greatest");
+elseif(B>A&&B>C)
+printf("no 2 is greatest");
+elseif(C>A&&C>B)
+printf("no 3 is greatest");
+elseif
+printf("it is not in format");}
